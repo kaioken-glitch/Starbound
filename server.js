87bb7a9a8b1cd870272df4b407749583fdbf7a51
kaioken-glitch <lightningcyranus@gmail.com>
2025-06-29 13:37:17 +0200

    catching a 502

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = 3001;
 
 // MongoDB connection
 const MONGODB_URI = process.env.MONGODB_URI;
+console.log('Loaded MONGODB_URI:', MONGODB_URI); // Debug: check if env var is loaded
 mongoose.connect(MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
